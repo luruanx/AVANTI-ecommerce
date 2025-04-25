e75101bcaf7edab99c12a379437a87d727e4b5b0
Ruan Richard <Richard_10luan@hotmail.com>
2025-04-25 17:49:49 -0300

    update carousel

--- assets/script.js
+++ assets/script.js
@@ -237,16 +237,20 @@ var swiper = new Swiper(".mySwiper", {
     el: ".swiper-pagination",
     clickable: true,
   },
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
   breakpoints: {
-    290: {
+    100: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    360: {
       slidesPerView: 2,
       spaceBetween: 10,
     },
