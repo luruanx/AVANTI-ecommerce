62aea987937ac971f11896722473d38901141e8a
Ruan Richard <Richard_10luan@hotmail.com>
2025-04-25 17:30:46 -0300

    autoplay js

--- assets/script.js
+++ assets/script.js
@@ -237,10 +237,10 @@ var swiper = new Swiper(".mySwiper", {
     el: ".swiper-pagination",
     clickable: true,
   },
-  // autoplay: {
-  //   delay: 2500,
-  //   disableOnInteraction: false,
-  // },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
